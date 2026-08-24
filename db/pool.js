54843f061346da451c0acc07e8e1aaa8bcc93bb3
Sess254 <wamaritemoses@gmail.com>
2026-08-24 15:02:14 +0300

    deploy3

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,16 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-module.exports = new Pool ({
-    host: "localhost",
-    user: "sess",
-    database: "inventory",
-    password: "sess",
-    port: 5432,
+// module.exports = new Pool ({
+//     host: "localhost",
+//     user: "sess",
+//     database: "inventory",
+//     password: "sess",
+//     port: 5432,
+// });
+
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  connectionString: "postgresql://sess:sess@localhost:5432/top_users"
 });
