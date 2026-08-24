c94a33d728909be69095b52064fabebf324bcd71
Sess254 <wamaritemoses@gmail.com>
2026-08-24 15:12:32 +0300

    deploy4

--- db/pool.js
+++ db/pool.js
@@ -8,9 +8,12 @@
 //     port: 5432,
 // });
 
-
 const { Pool } = require("pg");
+require("dotenv").config();
 
-module.exports = new Pool({
-  connectionString: "postgresql://sess:sess@localhost:5432/top_users"
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
+
+module.exports = pool;
